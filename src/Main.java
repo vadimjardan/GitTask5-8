59307eb51cafabf6b9fd7319c3dc1fd843097f7e
VJardan <vadim.jardan@endava.com>
2020-10-29 19:27:11 +0200

    add main class

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
         System.out.println("Hello, Git!");
         System.out.println("Let's be friends");
         System.out.println("Sorry for mistake in previous message!");
+
+        multi (5, 10);
     }
 
+    public static int multi (int a, int b)
+    { return a * b;}
 
 }
