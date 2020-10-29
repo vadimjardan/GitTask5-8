cb933513034ce14cc44dbdb54e3cd660427c57c9
VJardan <vadim.jardan@endava.com>
2020-10-29 18:53:26 +0200

    add main class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Git!");
-        System.out.println("Let be friends");
+        System.out.println("Let's be friends");
+        System.out.println("Sorry for mistake in previous message!");
     }
 
 
